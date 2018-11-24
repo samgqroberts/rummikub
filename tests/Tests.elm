7dfba0bf0f5365c627035f34e1efde799e5de4a0
Sam Roberts <samgqroberts@gmail.com>
2018-11-24 19:10:37 +0000

    add fuzzing to tests that generate a new game state

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -4,13 +4,14 @@ import Random
 import List.Extra exposing (getAt)
 import Game exposing (..)
 import Test exposing (..)
+import Fuzz exposing (..)
 import Models exposing (..)
 import Expect exposing (Expectation)
 
 
 -- Check out http://package.elm-lang.org/packages/elm-community/elm-test/latest to learn more about testing in Elm!
 
-newGameInst = newGame (Random.initialSeed 1)
+newGameInst seeder = newGame (Random.initialSeed seeder)
 
 expectStartingPlayerTileCount : Int -> GameState -> Expectation
 expectStartingPlayerTileCount playerIndex inst =
@@ -24,21 +25,21 @@ all =
     [ test "starting number of tiles" <|
         \_ ->
           Expect.equal (totalNumTiles) (List.length colors * List.length numbers * tileDuplicates)
-    , test "new game unflipped num" <|
-        \_ ->
-          Expect.equal (List.length newGameInst.unflipped) (totalNumTiles - startingPlayerTileCount * numPlayers)
-    , test "new game player num" <|
-        \_ ->
-          Expect.equal (List.length newGameInst.playerHands) (numPlayers)
-    , test "new game playerhand num tiles" <|
-        \_ ->
+    , fuzz int "new game unflipped num" <|
+        \seeder ->
+          Expect.equal (List.length (newGameInst seeder).unflipped) (totalNumTiles - startingPlayerTileCount * numPlayers)
+    , fuzz int "new game player num" <|
+        \seeder ->
+          Expect.equal (List.length (newGameInst seeder).playerHands) (numPlayers)
+    , fuzz int "new game playerhand num tiles" <|
+        \seeder ->
           Expect.all
             [ expectStartingPlayerTileCount 0
             , expectStartingPlayerTileCount 1
             , expectStartingPlayerTileCount 2
             , expectStartingPlayerTileCount 3
-            ] newGameInst
-    , test "new game has 0 played tiles" <|
-      \_ ->
-        Expect.equal (List.length newGameInst.played) 0
+            ] (newGameInst seeder)
+    , fuzz int "new game has 0 played tiles" <|
+        \seeder ->
+          Expect.equal (List.length (newGameInst seeder).played) 0
     ]

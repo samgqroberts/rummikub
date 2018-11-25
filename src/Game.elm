d8be656164ce382d665ef86a0e8209a719c1b07a
Sam Roberts <samgqroberts@gmail.com>
2018-11-25 03:45:46 +0000

    add isValidGroup fn and helper fns

--- src/Game.elm
+++ src/Game.elm
@@ -2,7 +2,7 @@ module Game exposing (..)
 
 import Random exposing (Seed)
 import List
-import List.Extra exposing (getAt)
+import List.Extra exposing (getAt, uniqueBy)
 import Tuple
 import Models exposing (Tile, Color(..), Number(..), Group, PlayerHand, GameState)
 
@@ -64,3 +64,79 @@ newGame seed =
     , playerHands = [playerHand1, playerHand2, playerHand3, playerHand4]
     , played = []
     }
+
+getColor : Tile -> Color
+getColor tile =
+  Tuple.first tile
+
+getNumber : Tile -> Number
+getNumber tile =
+  Tuple.second tile
+
+colorOrdinal : Color -> Int
+colorOrdinal color =
+  case color of
+    Black -> 0
+    Red -> 1
+    Orange -> 2
+    Blue -> 3
+
+numberOrdinal : Number -> Int
+numberOrdinal number =
+  case number of
+    One -> 1
+    Two -> 2
+    Three -> 3
+    Four -> 4
+    Five -> 5
+    Six -> 6
+    Seven -> 7
+    Eight -> 8
+    Nine -> 9
+    Ten -> 10
+    Eleven -> 11
+    Twelve -> 12
+    Thirteen -> 13
+
+allColorsUnique : Group -> Bool
+allColorsUnique group =
+  let
+    colorsInGroup = List.map getColor group
+  in
+    List.length (uniqueBy colorOrdinal colorsInGroup) == List.length colorsInGroup
+
+allColorsTheSame : Group -> Bool
+allColorsTheSame group =
+  case Maybe.map getColor (getAt 0 group) of
+    Nothing -> False
+    Just firstColor ->
+      List.all (\tile -> getColor tile == firstColor) group
+
+allNumbersTheSame : Group -> Bool
+allNumbersTheSame group =
+  case Maybe.map getNumber (getAt 0 group) of
+    Nothing -> False
+    Just firstNumber ->
+      List.all (\tile -> getNumber tile == firstNumber) group
+
+allNumbersSequential : Group -> Bool
+allNumbersSequential group =
+  let
+    sortedNumbers = List.map getNumber group
+      |> List.map numberOrdinal
+      |> List.sort
+  in
+    case List.head sortedNumbers of
+      Nothing -> True
+      Just firstNum ->
+        case List.head (List.reverse sortedNumbers) of
+          Nothing -> True
+          Just lastNum ->
+            lastNum - firstNum == List.length sortedNumbers - 1
+
+isValidGroup : Group -> Bool
+isValidGroup group =
+  List.length group > 2 && (
+    (allColorsUnique group && allNumbersTheSame group)
+    || (allColorsTheSame group && allNumbersSequential group)
+  )
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -42,4 +42,80 @@ all =
     , fuzz int "new game has 0 played tiles" <|
         \seeder ->
           Expect.equal (List.length (newGameInst seeder).played) 0
+    , describe "allColorsTheSame"
+      [ test "1 of the same color" <|
+          \_ ->
+            allColorsTheSame [(Black, One)] |> Expect.equal True
+      , test "2 of the same color" <|
+          \_ ->
+            allColorsTheSame [(Black, One), (Black, Two)] |> Expect.equal True
+      , test "3 of the same color" <|
+          \_ ->
+            allColorsTheSame [(Black, One), (Black, Two), (Black, Three)] |> Expect.equal True
+      , test "4 of the same color" <|
+          \_ ->
+            allColorsTheSame [(Black, One), (Black, Two), (Black, Three), (Black, One)] |> Expect.equal True
+      , test "3 of the same + 1 different" <|
+          \_ ->
+            allColorsTheSame [(Black, One), (Black, Two), (Black, Three), (Blue, One)] |> Expect.equal False
+      ]
+    , describe "allColorsUnique"
+      [ test "1 different colors" <|
+          \_ ->
+            allColorsUnique [(Black, One)] |> Expect.equal True
+      , test "2 different colors" <|
+          \_ ->
+            allColorsUnique [(Black, One), (Blue, One)] |> Expect.equal True
+      , test "3 different colors" <|
+          \_ ->
+            allColorsUnique [(Black, One), (Blue, Two), (Red, Three)] |> Expect.equal True
+      , test "4 different colors" <|
+          \_ ->
+            allColorsUnique [(Black, One), (Blue, Two), (Red, Three), (Orange, One)] |> Expect.equal True
+      , test "3 different + 1 duplicate" <|
+          \_ ->
+            allColorsUnique [(Black, One), (Blue, Two), (Black, Three), (Red, One)] |> Expect.equal False
+      , test "4 different + 1 duplicate" <|
+          \_ ->
+            allColorsUnique [(Black, One), (Blue, Two), (Black, Three), (Red, One), (Orange, Five)] |> Expect.equal False
+      ]
+    , describe "isValidGroup"
+      [ test "3 of the same number" <|
+          \_ ->
+            isValidGroup [(Black, One), (Blue, One), (Orange, One)] |> Expect.equal True
+      , test "4 of the same number" <|
+          \_ ->
+            isValidGroup [(Blue, Two), (Orange, Two), (Black, Two), (Red, Two)] |> Expect.equal True
+      , test "3 number run" <|
+          \_ ->
+            isValidGroup [(Blue, Five), (Blue, Four), (Blue, Six)] |> Expect.equal True
+      , test "13 number run" <|
+          \_ ->
+            isValidGroup [
+              (Blue, One), (Blue, Two), (Blue, Three), (Blue, Four), (Blue, Five),
+              (Blue, Six), (Blue, Seven), (Blue, Eight), (Blue, Nine), (Blue, Ten),
+              (Blue, Eleven), (Blue, Twelve), (Blue, Thirteen)
+            ] |> Expect.equal True
+      , test "run with mismatched color" <|
+          \_ ->
+            isValidGroup [(Orange, Ten), (Red, Eleven), (Orange, Twelve), (Orange, Thirteen)] |> Expect.equal False
+      , test "run with gap" <|
+          \_ ->
+            isValidGroup [(Red, Two), (Red, Four), (Red, Five), (Red, Six)] |> Expect.equal False
+      , test "3 of same number + 1 duplicate" <|
+          \_ ->
+            isValidGroup [(Red, Two), (Blue, Two), (Blue, Two), (Orange, Two)] |> Expect.equal False
+      , test "4 of same number + 1 duplicate" <|
+          \_ ->
+            isValidGroup [(Red, Two), (Blue, Two), (Blue, Two), (Orange, Two), (Black, Two)] |> Expect.equal False
+      , test "0 tile group" <|
+          \_ ->
+            isValidGroup [] |> Expect.equal False
+      , test "1 tile group" <|
+          \_ ->
+            isValidGroup [(Red, Two)] |> Expect.equal False
+      , test "2 tile group" <|
+          \_ ->
+            isValidGroup [(Red, Two), (Red, Three)] |> Expect.equal False
+      ]
     ]
